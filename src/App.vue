798963c4cf31c9ed4d42acfe3d81917034584f23
sfilhu <sfilhu@gmail.com>
2020-09-28 23:20:13 -0300

    nre layout seventeen

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,12 @@
 <template>
-  <v-app :style="{background: $vuetify.theme.themes[theme].background}">
-    <v-app-bar app color="primary" dark style="height: 40px;">
-      <v-switch v-model="themeSwitch" :label="theme" :dark="!themeSwitch"></v-switch>
-
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
-  </v-app>
+  <div id="app">
+    <router-view name="header" />
+    <div class="wrapper">
+      <router-view />
+    </div>
+    <router-view name="footer" />
+  </div>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld";
-
-export default {
-  name: "App",
-
-  components: {
-    HelloWorld,
-  },
-
-  watch: {
-    themeSwitch(val) {
-      this.$vuetify.theme.dark = val;
-    },
-  },
-
-  computed: {
-    theme() {
-      return this.$vuetify.theme.dark ? "dark" : "light";
-    },
-  },
-
-  data: () => ({
-    themeSwitch: true,
-  }),
-};
+export default {};
 </script>
